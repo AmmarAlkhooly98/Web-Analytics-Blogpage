bc8780bbdf1787d75d8d0d7e63c8080273365719
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2020-11-17 02:56:03 +0300

    added simple js code for fake signup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-const SignUp = () => {
 
+const signUp = () => {
+  
+  let name = document.getElementById("usr-name").value;
+  window.localStorage.setItem('userName', name)
+  let storedName = localStorage.getItem('userName')
+  // Get the element you want to add your new element before or after
+  var target = document.querySelector('#usr-inserted-name');
+  const content = target.innerHTML;
+
+  target.innerHTML = `Welcome, ${storedName.toString()}`;
+  // Create the new element
+  // This can be any valid HTML element: p, article, span, etc...
+  var h1 = document.createElement('h1');
+
+  // Add content to the new element
+  h1.innerHTML = 'Your content, markup, etc.';
 }
